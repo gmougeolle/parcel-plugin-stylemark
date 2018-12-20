07e32c004e26d5be99e7598b6fcf9f684ddeff12
Grégoire Mougeolle <gregoire@nextmap.io>
2018-12-20 14:48:58 +0100

    Changed input directory to include the whole project instead. Should probably add a config file for this.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const stylemark = require('stylemark');
 
 module.exports = function (bundler) {
    bundler.on('buildEnd', () => {
-        let opts = bundler.options;
-        let configPath = bundler.resolver.rootPackage.pkgdir + '/.stylemark.yml';
+      let opts = bundler.options;
+      let rootDir = bundler.resolver.rootPackage.pkgdir;
+      let configPath = rootDir + '/.stylemark.yml';
 
-        stylemark({input: opts.outDir, output: opts.outDir + '/stylemark', configPath});
+      stylemark({input: rootDir, output: opts.outDir + '/stylemark', configPath});
    })
 };
